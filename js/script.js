5fc165dd64f50f992291edf9a0ab4e97122ddf05
Priyankasapp <priyanka.s@applocum.org>
2026-05-18 13:18:28 +0530

    dark mode added on page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log("Food Landing Page Ready");
+const toggleBtn = document.getElementById('themeToggle');
+const body = document.body;
+
+// theme
+if (localStorage.getItem("theme") === "dark") {
+    body.classList.add("dark-mode");
+    toggleBtn.innerHTML = '<i class="bi bi-sun-fill"></i>';
+}
+
+toggleBtn.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+    if (body.classList.contains("dark-mode")) {
+        toggleBtn.innerHTML = '<i class="bi bi-sun-fill"></i>';
+        localStorage.setItem("theme", "dark");
+    } else {
+        toggleBtn.innerHTML = '<i class="bi bi-moon-fill"></i>';
+        localStorage.setItem("theme", "light");
+    }
+});
